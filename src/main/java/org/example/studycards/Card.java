8ff9b2f762cb3f8562df09c49da2c49d9e2c1efb
Matheus Gois <mattheusgoiis@gmail.com>
2025-07-05 23:25:30 -0300

    fix: refactor code smell using encapsulate field refactoring technique

--- src/main/java/org/example/studycards/Card.java
+++ src/main/java/org/example/studycards/Card.java
@@ -1,12 +1,15 @@
+// Card.java
 package org.example.studycards;
+
+import java.util.Objects;
 
 public class Card {
     private String question;
     private String answer;
 
     public Card(String question, String answer) {
-        this.question = question;
-        this.answer = answer;
+        setQuestion(question);
+        setAnswer(answer);
     }
 
     public String getQuestion() {
@@ -14,7 +17,7 @@ public class Card {
     }
 
     public void setQuestion(String question) {
-        this.question = question;
+        this.question = validateText(question, "question");
     }
 
     public String getAnswer() {
@@ -22,11 +25,40 @@ public class Card {
     }
 
     public void setAnswer(String answer) {
-        this.answer = answer;
+        this.answer = validateText(answer, "answer");
     }
 
     public void edit(String question, String answer) {
         setQuestion(question);
         setAnswer(answer);
+    }
+
+    private String validateText(String text, String fieldName) {
+        Objects.requireNonNull(text, fieldName + " cannot be null");
+        if (text.trim().isEmpty()) {
+            throw new IllegalArgumentException(fieldName + " cannot be empty");
+        }
+        return text.trim();
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "question='" + question + '\'' +
+                ", answer='" + answer + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Card)) return false;
+        Card other = (Card) obj;
+        return question.equals(other.question) && answer.equals(other.answer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(question, answer);
     }
 }
